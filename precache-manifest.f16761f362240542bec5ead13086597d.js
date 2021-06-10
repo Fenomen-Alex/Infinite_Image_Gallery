8aaf1ab50c9f2669f39c71b3565418eaf7f54b5a
Fenomen-Alex <alexfenomen@gmail.com>
2021-06-10 23:33:37 +0300

    Updates

---
+++ precache-manifest.f16761f362240542bec5ead13086597d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a06cf6778fef3639fa1c82177cf2b25",
+    "url": "/Infinite_Image_Gallery/index.html"
+  },
+  {
+    "revision": "ef55b5912a53f13717f1",
+    "url": "/Infinite_Image_Gallery/static/css/main.daef65c6.chunk.css"
+  },
+  {
+    "revision": "bbb496c077d9166323f6",
+    "url": "/Infinite_Image_Gallery/static/js/2.3176c888.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Infinite_Image_Gallery/static/js/2.3176c888.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef55b5912a53f13717f1",
+    "url": "/Infinite_Image_Gallery/static/js/main.17423354.chunk.js"
+  },
+  {
+    "revision": "07ae88fc54e095821994",
+    "url": "/Infinite_Image_Gallery/static/js/runtime-main.fd4f2d5f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Infinite_Image_Gallery/precache-manifest.f16761f362240542bec5ead13086597d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Infinite_Image_Gallery/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
